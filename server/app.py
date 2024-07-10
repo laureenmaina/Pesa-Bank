eae8251abef3405cfe3e7b0d3292d2e8e9216295
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-07-10 22:07:59 +0300

    Update Routes

--- server/app.py
+++ server/app.py
@@ -1,21 +1,12 @@
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+from models import db, User, Transaction
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pesabank.db'
-db = SQLAlchemy(app)
+db.init_app(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-
-class Transaction(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    amount = db.Column(db.Float, nullable=False)
-    type = db.Column(db.String(10), nullable=False)  
-
+ 
 @app.route('/signup', methods=['POST'])
 def signup():
     data = request.get_json()
